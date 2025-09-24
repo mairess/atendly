0ccb6145ff7cf61cc8f582ba3569d746d107f4af
mairess <trabalho.maires@gmail.com>
2025-09-24 12:14:31 -0300

    config: expose host on vite, set host var on compose

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,6 +12,10 @@ export default defineConfig({
     vueJsx(),
     vueDevTools(),
   ],
+  server: {
+    port: 5173,
+    host: true
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
